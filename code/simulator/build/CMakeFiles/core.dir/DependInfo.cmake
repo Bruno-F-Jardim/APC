de793a66775bed0a38e7be71775cc89f1be9f199
StrillX <brunofjm@gmail.com>
2025-11-24 19:06:30 +0000

    Added code repo, added generic abstract and started introduction

---
+++ code/simulator/build/CMakeFiles/core.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gr/Desktop/n_body/simulator/src/core/Body.cpp" "CMakeFiles/core.dir/src/core/Body.cpp.o" "gcc" "CMakeFiles/core.dir/src/core/Body.cpp.o.d"
+  "/home/gr/Desktop/n_body/simulator/src/core/PhysicsEngine.cpp" "CMakeFiles/core.dir/src/core/PhysicsEngine.cpp.o" "gcc" "CMakeFiles/core.dir/src/core/PhysicsEngine.cpp.o.d"
+  "/home/gr/Desktop/n_body/simulator/src/core/Universe.cpp" "CMakeFiles/core.dir/src/core/Universe.cpp.o" "gcc" "CMakeFiles/core.dir/src/core/Universe.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
