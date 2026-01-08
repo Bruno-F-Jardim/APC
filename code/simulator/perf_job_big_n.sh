8149eb1990426f597d16701fba3a6d2743f18ea8
AndreMendes21 <andre.f.mendes24@gmail.com>
2026-01-08 22:55:59 +0000

    Updated jobs

--- code/simulator/perf_job_big_n.sh
+++ code/simulator/perf_job_big_n.sh
@@ -11,7 +11,7 @@ module load cmake/3.22.0
 
 # 1. Compile with -g (needed for perf to map instructions to code)
 
-rm -r build/* test_results/*
+rm -r build/*
 mkdir -p test_results/seq_optimized
 cd build 
 cmake -DCMAKE_CXX_FLAGS="-O3" ..
