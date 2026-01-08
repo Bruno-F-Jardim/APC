50641c17eb2a4118ba44b7802d019c400180abd5
AndreMendes21 <andre.f.mendes24@gmail.com>
2026-01-08 22:56:29 +0000

    Update jobs

--- code/simulator/perf_job_small_n.sh
+++ code/simulator/perf_job_small_n.sh
@@ -11,7 +11,7 @@ module load cmake/3.22.0
 
 # 1. Compile with -g (needed for perf to map instructions to code)
 
-rm -r build/* test_results/seq_simd
+rm -r build/*
 mkdir -p test_results/seq_simd
 cd build 
 cmake -DCMAKE_CXX_FLAGS="-O3 -fopenmp" ..
